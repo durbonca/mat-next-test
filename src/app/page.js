16942aacf18613c75a3b0e03fd88bcfc4627f2a2
durbonca <durbonca@gmail.com>
2023-02-03 15:43:48 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -9,8 +9,12 @@ async function getCows() {
 export default async function Home() {
   const cows = await getCows();
   return (
-    <main className='w-full'>
+    <main className='w-full py-8'>
       <div className="flex items-center justify-center flex-col gap-5 p-4">
+      <h1 className='text-center text-6xl'>WE-LOVE-COWS.com !!!</h1>
+      <div className="rounded-xl overflow-hidden mb-10">
+        <iframe src="https://player.vimeo.com/video/795681841?h=f92d8b0312" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+      </ div>
         {cows.map( (cow) => (
           <div 
             className="flex items-center rounded-lg shadow-xl p-6 border-white border-2 w-1/2 flex-auto gap-4 cursor-pointer"
@@ -22,9 +26,6 @@ export default async function Home() {
           </div>
           )
         )}
-      <div className="rounded-xl overflow-hidden">
-        <iframe src="https://player.vimeo.com/video/795681841?h=f92d8b0312" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-      </ div>
       </div>
     </main>
   )
