76026856e357f469b90e6885e13349de5fa1c03e
durbonca <durbonca@gmail.com>
2023-02-03 15:29:18 -0300

    get cow

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,24 @@
 import styles from './page.module.css'
 
 async function getCows() {
-  const response = await fetch('https://mattel-back.onrender.com/api/cows', { next: { revalidate: 300 } })
+  const response = await fetch('https://mattel-back.onrender.com/api/cows?populate=img', { next: { revalidate: 300 } })
   const cows = await response.json()
   return cows.data
 }
 
 export default async function Home() {
   const cows = await getCows();
-
   return (
-    <main className={styles.main}>
-      <div className={styles.center}>
+    <main className='w-full'>
+      <div className="flex items-center justify-center flex-col gap-5 p-4">
         {cows.map( (cow) => (
-          <div key={cow.id} className={styles.cow}>
-            <h1>{cow.attributes.name}</h1>
-            <p>{cow.attributes.url}</p>
+          <div 
+            className="flex items-center rounded-lg shadow-xl p-6 border-white border-2 w-1/2 flex-auto gap-4 cursor-pointer"
+            key={cow.id} >
+            { cow.attributes.img &&
+              <img src={cow.attributes.img.data.attributes.url} height="100" width="100" alt="" />
+            }
+            <h1 className='text-6xl'>{cow.attributes.name}</h1>
           </div>
           )
         )}
